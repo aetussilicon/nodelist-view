2c55f4fadbeb26b1d97aee393bf8a2d397fba19b
Artur Oliveira <arturgaollidev@outlook.com>
2025-05-06 12:14:01 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="xs:mx-[10%]">
+        <div>
+            
+        </div>
+        <h1>Hello, world</h1>
+    </div>
+  )
+}
+
+export default App
